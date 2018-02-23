a11ebcbed91079ab301a31cac700b54a2c7a42cf
marius <mgerdes@uni-potsdam.de>
2018-02-23 04:13:47 +0100

    Comment block

--- iNeedConference.php
+++ iNeedConference.php
@@ -1,6 +1,18 @@
 
 <?php
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+/**
+ * @package iNeedConference
+ * @version 0.1
+ */
+/*
+Plugin Name: iNeedConference
+Plugin URI: http://wordpress.org/plugins/iNeedConference/
+Description: Simple plugin to organize a conference.
+Author: Marius Gerdes
+Version: 0.1
+Author URI: http://github.com/mjgerdes
+*/
 
 
 
