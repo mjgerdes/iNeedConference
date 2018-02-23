bb74ba8c388e85a693255e78327e328a530cf629
marius <mgerdes@uni-potsdam.de>
2018-02-23 22:12:41 +0100

    Fixed bug with return statement

--- inc-shortcodes.php
+++ inc-shortcodes.php
@@ -2,7 +2,7 @@
 
 function inc_get_register_attendee_shortcode($args) {
 
-return "<p>REIGSTER WORKS</p>"
+return "<p>REIGSTER WORKS</p>";
 }
 
 add_shortcode('register_attendee', 'inc_get_register_attendee_shortcode');
