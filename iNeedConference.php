d4befddbb399383739a7bd9f532ddad996fd6115
marius <mgerdes@uni-potsdam.de>
2018-02-24 10:00:27 +0100

    Added registration form, validation code and database insertion. Changed database table layout a little. Still need to decide what happens upon registration of same email twice. Also, form does not actually send validation email.

--- iNeedConference.php
+++ iNeedConference.php
@@ -32,6 +32,15 @@ return $wpdb->prefix . '_attendee';
 /* Creates database tables if none already exist.
 This function is run upon plugin activation.
 */
+
+function inc_attendee_status_code($n) {
+$phplol =  array( -1 => "await_email_validation",
+ 0 => "await_payment",
+ 1 => "attendee",
+ 666 => "needs_attention");
+return $phplol[$n];
+}
+
 function inc_install() {
 global $wpdb;
 	global $inc_db_version;
@@ -39,12 +48,14 @@ global $wpdb;
 $table_name = inc_attendee_table_name();
 $charset_collate = $wpdb->get_charset_collate();
 
+$default_status = inc_attendee_status_code(-1);
 $sql = "CREATE TABLE $table_name (
   id mediumint(9) NOT NULL AUTO_INCREMENT,
   time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+  auth tinytext NOT NULL DEFAULT '',
   name tinytext NOT NULL,
   email varchar(255) DEFAULT '' NOT NULL,
-  payment_complete bool NOT NULL DEFAULT 0,
+  status tinytext NOT NULL DEFAULT '',
   note text NOT NULL DEFAULT '',
   PRIMARY KEY  (id)
 ) $charset_collate;";
@@ -59,6 +70,15 @@ dbDelta( $sql );
 
 // we only want to check for tables at plugin activation
 register_activation_hook(__FILE__, 'inc_install');
+
+
+/* this is what we put in the auth field in the attendee table.
+It's not really necessary, which is hilarious, and we never check it, but it has to be there or the auth string we give to users won't look like an auth string!
+Also, I suppose it serves the function of preventing people from figuring out the registration status of other users. */
+function inc_generate_auth() {
+$length = 5;
+    return substr(str_shuffle(str_repeat($x='ABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil($length/strlen($x)) )),1,$length);
+}
 
 
 include('inc-shortcodes.php');
--- inc-register.template.php
+++ inc-register.template.php
@@ -1,3 +1,61 @@
 
-<p>Please fill out the following.</p>
-<form
+<?php	
+$success = false;
+$message = "Please, fill out the information below.";
+
+if(isset($_POST["submit_form"])) {
+  // we are here after user clicks submit
+
+    $attendee_name = strip_tags($_POST["attendee_name"], "");
+if ($attendee_name == "") {
+// case if name was left blank
+$message = "<b>Please, don't be a stranger. Tell us your name!</b>";
+}else if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+ // case if no valid email provided
+$message = "<b>Please provide a valid email adress!</b>";
+} else {
+// all good, insert into db
+global $wpdb;
+$table_name = inc_attendee_table_name();
+
+    $wpdb->insert( 
+        $table_name, 
+        array(
+		"time" => current_time('mysql'),
+		"auth" => inc_generate_auth(),
+            'name' => $attendee_name,
+			"email" => $_POST['email'],
+			"status" => inc_attendee_status_code(-1),
+			"note" => strip_tags($_POST['note'], "")
+        )
+    );
+$success = true;
+}
+}
+
+// form
+if($success) {
+echo "<p>Thank you $attendee_name, we are looking forward to meeting you! Please check your email and follow the instructions provided in the validation mail that our robots have just sent to you. Remember, if you have any further questions about your registration, feel free to reach out to us at help@tacos28.de</p>";
+} else {
+
+echo "<p>$message</p>";
+
+?>
+
+<br/>
+<form action="#validate_form" method="post" id="validate_form">
+    <label for="attendee_name"><h3>Your name</h3></label>
+    <input required type="text" name="attendee_name" id="attendee_name" />
+
+<label for="email"><h3>Your E-Mail adress</h3></label>
+    <input required type="email" name="email" id="email" />
+
+<label for="note"><h3>Anything you would like to tell us</h3></label>
+<input type="textarea" name="note" id="note" />
+
+<input type="submit" name="submit_form" value="submit" />
+</form>
+
+<?php
+}
+?>
