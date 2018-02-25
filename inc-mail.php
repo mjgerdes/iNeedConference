4619583f8bf361c755cbcd915bc2af1ce5bd23fc
marius <mgerdes@uni-potsdam.de>
2018-02-25 07:19:53 +0100

    Added mail functionality.

---
+++ inc-mail.php
@@ -0,0 +1,32 @@
+
+<?php
+
+
+/* Should get the base domain name for sending mails with noreplay…@domain.tld */
+function inc_site_domain() {
+return parse_url($_SERVER['SERVER_NAME'], PHP_URL_HOST);
+}
+
+function inc_send_mail($to, $subject, $body) {
+$domain = inc_site_domain();
+$additional_headers = "from: noreply@$domain\nReply-to: noreply@$domain";
+return mail($to, $subject, $msg, $additional_headers);
+}
+
+function inc_send_attendee_validation_mail($attendee_name, $attendee_mail, $authcode) {
+$ body = "Dear $attendee_name,\n"
+. thank you for registering to attend to the 28th TaCoS, which will be held on June 8th to June 10th at the University of Potsdam.\n"
+. "To confirm your registration attempt, please visit the link below.\n"
+. "\n"
+. get_site_url() . "/register-to-attend?authcode=" . $authcode
+. "\n"
+. "Your authentication code is:\n$authcode\n"
+. "After validating your registration, use this code, or visit the above URL again, to check on the status of your account.\n"
+. "To complete your registration process, please send $10 to the following bank accFIXME: insert real text here\n"
+. "\n"
+. "We are looking forward to meeting you!\nSincerely, the TaCoS28 Mail Robot\n";
+return inc_send_mail($attendee_mail, "Thank you for registering to attend TaCoS28!", $body);
+}
+
+
+?>
