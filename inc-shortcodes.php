a06aeb602a9c39671d306c9eff27d7f6f68e6ee0
marius <mgerdes@uni-potsdam.de>
2018-03-05 04:53:51 +0100

    Change small auth shortcode to show button directly next to input field.

--- inc-shortcodes.php
+++ inc-shortcodes.php
@@ -16,7 +16,8 @@ function inc_get_small_auth_shortcode() {
 $url = get_site_url() . "/register-to-attend";
 echo "<form method='get' action='$url'>";
 echo "<label for='small_auth'>Authentication Code</label>"
-. "<input type='text' id='small_auth' name='authcode' placeholder='i.e. 5-QRCDB' />";
+. '<input type="text" id="small_auth" name="authcode" placeholder="i.e. 5-QRCDB" value="" style="display: block;  width: 100px; height: 32px; margin: 0px; padding: 0px; float: left;" />'
+. '<input type="submit" value="Check" style="display: block; margin: 0px;  width: 40px; height: 34px; padding: 0px; " />';
 echo "</form>";
 
 
