ad619a44a551fe402bce4117fcaeabbebd478f15
marius <mgerdes@uni-potsdam.de>
2018-05-12 23:19:21 +0200

    Add DB functions (insert, update, delete and retrieve) for couches.

--- inc-couches-db.php
+++ inc-couches-db.php
@@ -4,6 +4,38 @@
 Functions for handling couches as db objects
 */
 
+function inc_couches_all() {
+global $wpdb;
+$table_name = inc_couches_table_name();
+
+return $wpdb->get_results("
+SELECT * FROM $table_name");
+}
+
+function inc_couches() {
+global $wpdb;
+$table_name = inc_couches_table_name();
+
+return $wpdb->get_results("
+SELECT * FROM $table_name WHERE deleted = 0");
+}
+
+function inc_couches_from_id($id) {
+global $wpdb;
+$table_name = inc_couches_table_name();
+
+$phplol = $wpdb->get_results($wpdb->prepare("
+SELECT * FROM $table_name
+ WHERE id = %d",
+$id));
+
+if($phplol) {
+return $phplol[0];
+}
+
+return NULL;
+}
+
 function inccouches_insert_from_valid($validEmail, $validLocation, $validDescription) {
 global $wpdb;
 $table_name = inc_couches_table_name();
@@ -26,5 +58,29 @@ return NULL;
 return $wpdb->insert_id;
 }
 
+// this is more for admin purposes
+function inc_couches_update_unsafe($couch) {
+global $wpdb;
+
+$res = $wpdb->update(inc_couches_table_name(),
+(array)$couch,
+array( "id" => $couch->id));
+
+// as always, no idea what to do if this fails
+return $res;
+}
+
+function inc_couches_delete($couch) {
+global $wpdb;
+$couch->deleted = 1;
+
+$res = $wpdb->update(inc_couches_table_name(),
+array( "deleted" => $couch->deleted),
+array( "id" => $couch->id),
+array("%s"));
+
+// as always, no idea what to do if this fails
+return $res;
+}
 
 ?>
