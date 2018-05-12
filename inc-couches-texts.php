ccd6e3bb2f3956e7fe396149471bf206d0dabb21
marius <mgerdes@uni-potsdam.de>
2018-05-13 00:45:49 +0200

    Add basic couch processing skeleton.\nAdd looking-for-couch form.\nAdd looking-for-couch processing functions.

--- inc-couches-texts.php
+++ inc-couches-texts.php
@@ -9,4 +9,18 @@ function inc_couches_text_disclaimer() {
    . "<p>The TaCoS 28 organisation team assumes no responsibility for the quality, availability or basic serviceability of sleeping arrangements that are negotiated through this service. We offer only a platform for students to arrange lodging amongst themselves, and are involved only insofar as we participate by offering our own couches (i.e. sleeping accomodations) on this platform. Problems with arrangements must be taken up with the respective party of any sleeping arrangement, contract or agreement of lodging made through this service.</p>";
 }
 
+function inc_couches_texts_looking_explain() {
+return "<p>Enter your auth code below to get an overview of possible accomodations by volunteering students from the Potsdam/Berlin area. You received an authcode as part of your registration. You must be <a href='http://tacos28.de/register-to-attend'>registered</a> and have a validated account to see this information.</p>"; 
+}
+
+function inc_couches_texts_overview_explain($attendee) {
+return "<h3>The Couch Bazaar</h3>"
+. "<p>There you go, $attendee->name! Below you will find a table of E-Mail addresses of fellow students who have offered to provide lodging to TaCoS 28 attendees like you. If you wish to take up the offer, then we ask that you contact these kind people o your own initiative and responsibility. Please understand that, while we try to keep this list as up-to-date as possible, you may have to try contacting several people.</p>";
+}
+
+function inc_couches_texts_overview_none() {
+return "<br/><p>Sorry, it seems that there is no one offering right now. We are always actively pursuing more possibilities for accomodation, so please check again soon!</p>";
+}
+
+
 ?>
--- inc-couches.php
+++ inc-couches.php
@@ -7,8 +7,104 @@ include('inc-couches-db.php');
 
 function inc_get_couches_shortcode() {
 $out = "";
+
+if(isset($_POST['looking_submit'])) {
+$res = inc_couches_looking_process();
+$out .= $res[0];
+$success = $res[1];
+
+if($success) {
+// this is all we do for the page in this case
+return $out;
+}
+// if failure, then error message is already in $out
+}
+
+// default functions of page
+$out .= "<h2>I am ...</h2>"
+. inc_couches_looking()
+. "<br/>"
+. inc_couches_offering()
+. "<br/>"
+. inc_couches_deleting()
+. "<br/>";
+
 $out .= inc_couches_text_disclaimer();
 return $out;
 }
 
+function inc_couches_looking() {
+$out = "";
+
+$out .= "<h3>... looking for a place to stay in the Potsdam/Berlin area!</h3>"
+. inc_couches_texts_looking_explain();
+
+$out .= "<form method='POST'  id='looking_form'>"
+. "<label for='looking_authcode'>Authentication Code</label>"
+. "<input type='text' name='looking_authcode' name='looking_authcode' placeholder='e.g. 5-QIFES' />"
+. "<br/><button type='submit' name='looking_submit' value='looking_submit'>Show me the couches!</button>"
+. "</form>";
+
+return $out;
+}
+
+// this function returns two values, a string (the usual out) and a bool indicating success or failure
+// the intent is to reprint the default functions of the page on failure
+function inc_couches_looking_process() {
+$out = "";
+
+if(!isset($_POST['looking_authcode']) || $_POST['looking_authcode'] == "") {
+$out .= "<p>Please provide a valid authentication code.</p>";
+return array($out, false);
+}
+
+$attendee = inc_attendee_from_authcode(inc_attendee_authcode_from_string($_POST['looking_authcode']));
+
+if(!$attendee) {
+$out .= "<p>The provided authcode appears to be invalid.</p>";
+return array($out, false);
+}
+
+if($attendee->status == inc_attendee_status_code(INC_ATTENDEE_STATUS_AWAIT_EMAIL_VALIDATION)) {
+$out .= "<p>Sorry $attendee->name, you must first validate your account as described in your validation email before you can view the couch surfing bazaar.</p>";
+return array($out, false);
+}
+
+// all seems good
+$out .= inc_couches_texts_overview_explain($attendee);
+$couches = inc_couches();
+if(!$couches) {
+$out .= inc_couches_texts_overview_none();
+return array($out, true);
+}
+
+$out .= "<table>"
+. "<th>E-Mail</th>"
+. "<th>Location</th>"
+. "<th>Description</th>";
+
+foreach($couches as $couch) {
+// email
+$out .= "<td>$couch->email</td>"
+// location
+. "<td>$couch->location</td>"
+// description
+. "<td>$couch->description</td>";
+}
+$out .= "</table>";
+
+return array($out, true);
+}
+
+function inc_couches_offering() {
+$out = "";
+
+return $out;
+}
+
+function inc_couches_deleting() {
+$out = "";
+
+return $out;
+}
 ?>
